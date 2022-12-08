db4cffbef737012737034a5e38696e06b6e84c78
mscyzertx <88049001+mscyzertx@users.noreply.github.com>
2022-12-08 15:42:44 -0500

    background url update

--- webapp/app.py
+++ webapp/app.py
@@ -14,7 +14,7 @@ DBPWD = os.environ.get("DBPWD") or "password"
 DATABASE = os.environ.get("DATABASE") or "employees"
 DBPORT = int(os.environ.get("DBPORT"))
 GROUP_NAME = os.environ.get('GROUP_NAME')
-BACKGROUND_URL = os.environ.get('BACKGROUND_URL')
+BACKGROUND_URL = os.environ.get('image') or "failed to load"
     
 
 # Create a connection to the MySQL database
@@ -47,24 +47,26 @@ table = 'employee';
 # # Generate a random color
 # COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
 
-def backgroud_file(file_name, bucket):
+def background_file(backgroundimg1, bucket):
     """
     Function to download a given file from an S3 bucket
     """
     s3 = boto3.resource('s3')
-    output = f"background/{file_name}"
-    s3.Bucket(bucket).backgroud_file(file_name, output)
+    output = f"background/{backgroundimg1}"
+    s3.Bucket(bucket).background_file(backgroundimg1, output)
 
     return output
+
+BACKGROUND = "background/backgroundimg1"
 
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('addemp.html', GROUP_NAME=GROUP_NAME)
+    return render_template('addemp.html', GROUP_NAME=GROUP_NAME, background=BACKGROUND)
 
 @app.route("/about", methods=['GET','POST' ])
 def about():
-    return render_template('about.html', GROUP_NAME=GROUP_NAME)
+    return render_template('about.html', GROUP_NAME=GROUP_NAME, background=BACKGROUND)
 
     
 @app.route("/addemp", methods=['POST'])
@@ -93,7 +95,7 @@ def AddEmp():
 
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
-    return render_template("getemp.html", GROUP_NAME=GROUP_NAME)
+    return render_template("getemp.html", GROUP_NAME=GROUP_NAME, background=BACKGROUND)
 
 
 @app.route("/fetchdata", methods=['GET','POST'])
@@ -122,7 +124,7 @@ def FetchData():
         cursor.close()
 
     return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
-                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"],GROUP_NAME=GROUP_NAME, image1=image1)
+                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"],GROUP_NAME=GROUP_NAME, background=BACKGROUND)
 
 if __name__ == '__main__':
     
